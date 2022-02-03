d1394c7dc56a5418b2cfefa376300a60b9493a16
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-02-03 09:30:00 -0400

    Adiciona exercicios

---
+++ 1- Fundamentos/Bloco_04_Introdução_JavaScript_Lógica_Programação/dia_01_Introdução_JavaScript/Exercicio.js
@@ -0,0 +1,23 @@
+const a = 10;
+const b = 20;
+const c = 30;
+const d = 40;
+let result;
+let e = "negative";
+
+/*console.log("Adição", a+b);
+console.log("Subtração", a-b);
+console.log("Multiplicação", a*b);
+console.log("Divisão", a/b);
+console.log("Módulo", a%b);*/
+
+switch(e){
+    case "positive":
+      console.log("positive");
+      break;
+    case "negative":
+        console.log("negative");
+        break;
+    default:
+        console.log("zero");  
+}
