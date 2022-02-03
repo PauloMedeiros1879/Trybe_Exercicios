8956fc3633a2ad6f056b46d6815d521c524daadb
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-02-03 10:41:31 -0400

    Resolvendo exercicios 1,2,3,4 do arquivo Exercicios.js

--- 1- Fundamentos/Bloco_04_Introdução_JavaScript_Lógica_Programação/dia_01_Introdução_JavaScript/Exercicio.js
+++ 1- Fundamentos/Bloco_04_Introdução_JavaScript_Lógica_Programação/dia_01_Introdução_JavaScript/Exercicio.js
@@ -1,15 +1,43 @@
+//Exercicio 01
+
 const a = 10;
 const b = 20;
-const c = 30;
-const d = 40;
-let result;
-let e = "negative";
 
-/*console.log("Adição", a+b);
+console.log("Adição", a+b);
 console.log("Subtração", a-b);
 console.log("Multiplicação", a*b);
 console.log("Divisão", a/b);
-console.log("Módulo", a%b);*/
+console.log("Módulo", a%b);
+
+//Exercicio 02
+
+let one = 2;
+let two = 4;
+
+if(one >= two){
+    console.log("One é maior");
+}
+else if(two >= one){
+    console.log("Two é maior")
+}
+
+//Exercicio 03
+
+const first = 10;
+const second = 20;
+const third = 30;
+
+if(first >= second && first >= third){
+    console.log("first é maior");
+} else if(second >= first && second >= third){
+    console.log("second é maior");
+} else if(third >= first && third >= second){
+    console.log("third é maior");
+}
+
+//Exercicio 04
+
+let e = "negative";
 
 switch(e){
     case "positive":
@@ -21,3 +49,19 @@ switch(e){
     default:
         console.log("zero");  
 }
+
+//Exercicio 05
+
+
+//Exercicio 06
+
+//Exercicio 07
+
+//Exercicio 08
+
+//Exercicio 09
+
+//Exercicio 10
+
+//Exercicio 11
+
