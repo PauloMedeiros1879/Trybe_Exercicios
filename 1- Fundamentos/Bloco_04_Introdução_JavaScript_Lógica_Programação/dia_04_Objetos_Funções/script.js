9f73600ec0af418409481068c8e2e5c9d03d2b9a
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-02-07 17:42:39 -0400

    Adiciona fixaçoes 4.4

--- 1- Fundamentos/Bloco_04_Introdução_JavaScript_Lógica_Programação/dia_04_Objetos_Funções/script.js
+++ 1- Fundamentos/Bloco_04_Introdução_JavaScript_Lógica_Programação/dia_04_Objetos_Funções/script.js
@@ -69,3 +69,22 @@ console.log('Olá' + ' ' + names[key]);
 for(let key in car){
 console.log(key, car[key]);
 }
+
+//Fixação - Função
+
+/*EX:
+
+function maiorNum(primeiroNum, segundoNum) {
+    if (primeiroNum > segundoNum) {
+        return primeiroNum + ' é maior que ' + segundoNum;
+      } else if (segundoNum > primeiroNum) {
+        return segundoNum + ' é maior que ' + primeiroNum;
+      } else {
+        return 'Os números são iguais';
+      }
+    }
+    
+    console.log(maiorNum(10, 20)); // 20 é maior que 10
+    console.log(maiorNum(2, -5)); // 2 é maior que -5
+    console.log(maiorNum(1, 1)); // Os números são iguais
+    */
