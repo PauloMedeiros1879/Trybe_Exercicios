b85b2775c5aa2c72985c39a6ace0ca695758dc22
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-03 17:36:57 -0400

    Adiciona exercicio 2 - dia 01

--- 1- Fundamentos/Bloco_07_Introdução_JS-ES6_Testes-Unitários/dia_01-JS-ES6_let_const_arrow-functions_template-literals/script.js
+++ 1- Fundamentos/Bloco_07_Introdução_JS-ES6_Testes-Unitários/dia_01-JS-ES6_let_const_arrow-functions_template-literals/script.js
@@ -1,3 +1,5 @@
+// Exercício 1
+
 const testingScope = escopo => { 
     if (escopo === true) { 
       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
@@ -11,3 +13,9 @@ const testingScope = escopo => {
   }
 
   testingScope(true);
+
+  // Exercício 2
+
+  const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+oddsAndEvens.sort((a, b) => a - b);
+console.log(oddsAndEvens);
