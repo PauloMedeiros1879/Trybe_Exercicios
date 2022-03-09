dc91eb11b030b1c0fe6e0644c698d915852491c8
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-09 17:08:57 -0400

    Adiciona correçoes

--- 1- Fundamentos/Bloco_08_Higher_Order_Functions_JS-ES6/dia_01_JS-ES6_Introdução/exercise.js
+++ 1- Fundamentos/Bloco_08_Higher_Order_Functions_JS-ES6/dia_01_JS-ES6_Introdução/exercise.js
@@ -4,12 +4,12 @@ const employees = (names) => {
   return { names, email: `${email}@trybe.com` };
 };
 const newEmployees = (callback) => {
-    const employees = {
+    const employeesApply = {
       id1: callback('Pedro Guerra'), // Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro, substituindo as aspas
       id2: callback('Luiza Drumond'), // Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro, substituindo as aspas
       id3: callback('Carla Paiva'), // Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro, substituindo as aspas
     }
-    return employees;
+    return employeesApply;
   };
 
   console.log(newEmployees(employees));
