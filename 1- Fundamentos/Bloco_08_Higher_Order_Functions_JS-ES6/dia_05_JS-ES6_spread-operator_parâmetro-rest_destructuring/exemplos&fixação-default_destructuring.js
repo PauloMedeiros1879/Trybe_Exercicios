0d9096abee01011b0818331f5a6b831c767873f2
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-15 15:08:15 -0400

    Adiciona fixaçao&exemplos default destructuring

---
+++ 1- Fundamentos/Bloco_08_Higher_Order_Functions_JS-ES6/dia_05_JS-ES6_spread-operator_parâmetro-rest_destructuring/exemplos&fixação-default_destructuring.js
@@ -0,0 +1,55 @@
+//course test
+const persona = {
+    name: 'João',
+    lastName: 'Jr',
+    age: 34,
+  };
+  
+  const { nationalitys } = persona;
+
+  console.log(nationalitys);
+
+
+//default destructuring object
+/*
+const person = {
+    name: 'João',
+    lastName: 'Jr',
+    age: 34,
+  };
+  
+  const { nationality = 'Brazilian' } = person;
+  console.log(nationality); // Brazilian
+*/
+
+
+//default destructuring array
+/*
+const position2d = [1.0, 2.0];
+const [x, y, z = 0] = position2d;
+
+console.log(x); // 1
+console.log(y); // 2
+console.log(z); // 0
+*/
+
+
+//----------Fixação---------
+
+//Do jeito que está, quando passamos person para a função GetNationality o retorno é João is undefined . Ajuste a função GetNationality para que a chamada GetNationality(person) retorne João is Brazilian .
+
+const getNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const person = {
+  firstName: 'João',
+  lastName: 'Jr II',
+};
+
+const otherPerson = {
+  firstName: 'Ivan',
+  lastName: 'Ivanovich',
+  nationality: 'Russian',
+};
+
+console.log(getNationality(otherPerson)); // Ivan is Russian
+console.log(getNationality(person));
