baeb652d25cd8eb4b1f6c4eec33dbed9b4351cd7
Paulo_Medeiros <paulo_alexandre_slipknot@hotmail.com>
2022-03-23 13:51:58 -0400

    Adiciona exemplos do dia 02

---
+++ 1- Fundamentos/Bloco_09_Assincronicidade&Callbacks/dia_02_JS_Assíncrono_Fetch-API_async-await/Fetch API/fetch.js
@@ -0,0 +1,42 @@
+//--------------------- parte comentada está em pending, sem o then()--------------------------
+
+// const fetch = require('node-fetch');
+
+// const fetchJoke = () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   console.log(fetch(url));
+// }
+
+// fetchJoke();
+
+// -------------aqui adiciona o .then()----------------------
+
+// const fetch = require('node-fetch');
+
+// const fetchJoke = () => {
+//   const url = 'https://api.chucknorris.io/jokes/random?category=dev';
+
+//   fetch(url)
+//     .then((response) => response.json())
+//     .then((data) => console.log(data.value));
+// }
+
+// fetchJoke();
+
+// --------------------usando o catch e forçando o erro removendo o https-----------------------
+const fetch = require('node-fetch');
+
+const fetchJoke = () => {
+  const url = 'api.chucknorris.io/jokes/random?category=dev';
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => console.log(data.value))
+    .catch((error) => console.log(`Algo deu errado :( \n${error}`));
+}
+
+fetchJoke();
+
+// Algo deu errado :( 
+// TypeError: Only absolute URLs are supported
